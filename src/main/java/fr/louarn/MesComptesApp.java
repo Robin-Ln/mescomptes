7ab8344fef31295a9ffd3feb06538f4452317a49
Robin LOUARN <robin.louarn@icloud.com>
2019-08-10 13:48:41 +0200

    [RLO] Configuration configuration de log4j

--- src/main/java/fr/louarn/MesComptesApp.java
+++ src/main/java/fr/louarn/MesComptesApp.java
@@ -1,18 +1,20 @@
 package fr.louarn;
 
 import fr.louarn.dto.OperationDto;
+import lombok.extern.log4j.Log4j;
 
 import java.math.BigDecimal;
 
+@Log4j
 public class MesComptesApp {
 
     public static void main(String[] args) {
-        System.out.println("test");
-        OperationDto.builder()
+        log.info(OperationDto.builder()
                 .date("14/03/1996")
                 .libelle("Macdo")
                 .montantEur(BigDecimal.valueOf(12.4))
-                .build();
+                .build());
+
     }
 
 }
--- src/main/java/fr/louarn/dto/OperationDto.java
+++ src/main/java/fr/louarn/dto/OperationDto.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @Builder
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
