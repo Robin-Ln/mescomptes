1be0c3e0e2db38f91024facf51627892f8ee4786
Robin LOUARN <robin.louarn@icloud.com>
2019-08-15 23:10:25 +0200

    [RLO] Documentation de l'API

--- src/main/java/fr/louarn/mescomptes/controller/IOperationController.java
+++ src/main/java/fr/louarn/mescomptes/controller/IOperationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Api(value = "Action de base sur une opérations")
+@Api(value = "/operation", tags = "Operation")
 @RequestMapping(path = "/operation")
 public interface IOperationController {
 
